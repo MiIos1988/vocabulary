9012390107b6b83e29713f48b02c02f94575ae8c
Milos1988 <vojvoda1988@gmail.com>
2023-06-15 12:20:49 +0200

    Create backend folder

---
+++ backend/readDocx.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const mammoth = require('mammoth');
+
+async function readDocx(files) {
+  
+}
+
+module.exports = { readDocx }
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const PORT = 5500;
+const fs = require('fs');
+const path = require('path');
+const { readDocx } = require('./readDocx');
+
+const app = express();
+
+async function prepareFile() {
+    const allFiles = await fs.promises.readdir(
+        path.join(__dirname, 'initialFile')
+    );
+
+    const docxFiles = allFiles.filter(file => file.endsWith("docx"))
+    const docxData = await readDocx(docxFiles)
+}
+
+prepareFile()
+
+
+
+
+app.listen(PORT, (error) => {
+    error ? console.error("Error on server start") :
+        console.log(`Server running on port ${PORT} ...`);
+});
