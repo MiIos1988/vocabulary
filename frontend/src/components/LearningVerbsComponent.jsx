740c9b45e26ecfb48537c49b4dd093caabf6809b
Milos1988 <vojvoda1988@gmail.com>
2023-06-16 12:42:02 +0200

    Finished learning page

--- frontend/src/components/LearningVerbsComponent.jsx
+++ frontend/src/components/LearningVerbsComponent.jsx
@@ -3,16 +3,51 @@ import { useSelector } from 'react-redux'
 import { getVerbs } from '../services/getData'
 
 const LearningVerbsComponent = () => {
-    const [arrayVerbs, setArrayVerbs ] = useState([])
+  const [arrayVerbs, setArrayVerbs] = useState([]);
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [randomString, setRandomString] = useState("run - trčati (rʌn)")
+  const [randomWord, setRandomWord] = useState("trčati");
+  const [singleWord, setSingleWord] = useState(true);
 
-    useEffect(() => {
-        getVerbs().then(res => setArrayVerbs(res.data))
-    }, []
-    )
-    
+  useEffect(() => {
+    getVerbs().then(res => setArrayVerbs(res.data))
+  }, []
+  )
+
+  const writeOneWord = (string) => {
+    let start_index = string.indexOf("-") + 2;
+    let end_index = string.indexOf("(") - 1;
+    let b = string.substring(start_index, end_index);
+    setRandomWord(b)
+  }
+
   return (
     <div>
-      <h1>{arrayVerbs[15]}</h1>
+      {singleWord ? <div className='d-flex justify-content-center mt-5'>
+        <h2>{randomWord}</h2>
+        <button className='btn btn-primary ms-5' onClick={() => {
+          setSingleWord(false);
+          const random = Math.floor(Math.random() * arrayVerbs.length)
+          setRandomNumber(random);
+
+
+        }}>View translate</button>
+      </div>
+        : <>
+          <div className='d-flex justify-content-center mt-5'>
+            <h2>{randomString}</h2>
+            <button className='btn btn-primary ms-5' onClick={() => {
+              const a = arrayVerbs[randomNumber];
+              setRandomString(a)
+              writeOneWord(a)
+              setSingleWord(true)
+
+            }}>Next verb</button>
+          </div>
+          <input className='form-control m-5' type="text" placeholder='Write a sentence using this verb!' />
+        </>
+      }
+
     </div>
   )
 }
