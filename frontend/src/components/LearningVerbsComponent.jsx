8267fe537ab5e7f71987a00f64814cec98effa5a
Milos1988 <vojvoda1988@gmail.com>
2023-06-17 15:26:28 +0200

    change app

--- frontend/src/components/LearningVerbsComponent.jsx
+++ frontend/src/components/LearningVerbsComponent.jsx
@@ -1,57 +1,104 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { getVerbs } from '../services/getData'
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { getVerbs } from "../services/getData";
 
 const LearningVerbsComponent = () => {
   const [arrayVerbs, setArrayVerbs] = useState([]);
   const [randomNumber, setRandomNumber] = useState(0);
-  const [randomString, setRandomString] = useState("run - trčati (rʌn)")
+  const [randomString, setRandomString] = useState("run - trčati (rʌn)");
   const [randomWord, setRandomWord] = useState("trčati");
   const [singleWord, setSingleWord] = useState(true);
+  const [attempt, setAttempt] = useState('')
+  const inputRef = useRef();
 
   useEffect(() => {
-    getVerbs().then(res => setArrayVerbs(res.data))
-  }, []
-  )
+    getVerbs().then((res) => setArrayVerbs(res.data));
+  }, []);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [singleWord]);
 
   const writeOneWord = (string) => {
     let start_index = string.indexOf("-") + 2;
     let end_index = string.indexOf("(") - 1;
     let oneWord = string.substring(start_index, end_index);
-    setRandomWord(oneWord)
+    setRandomWord(oneWord);
+  };
+
+  const handlerPressKey = (e, isTrue) => {
+   if( e.key === "Enter") {
+    if(!isTrue){
+     setSingleWord(false);
+     const random = Math.floor(Math.random() * arrayVerbs.length);
+    setRandomNumber(random);
+    setAttempt('');
+    }else{
+    const newString = arrayVerbs[randomNumber];
+    setRandomString(newString);
+    writeOneWord(newString);
+    setSingleWord(true);
+   }
   }
+  };
 
   return (
-    <div className='container'>
-      {singleWord ? 
-      <div>
-        <div className='d-flex justify-content-center mt-5'>
-        <h2>{randomWord}</h2>
-        <button className='btn btn-primary ms-5' onClick={() => {
-          setSingleWord(false);
-          const random = Math.floor(Math.random() * arrayVerbs.length)
-          setRandomNumber(random);
-        }}>View translate</button>
-        </div>
-        <input className='form-control my-4' type="text" placeholder='Write a sentence using this verb!' />
-      </div>
-        : <>
-          <div className='d-lg-flex justify-content-center mt-5 text-center'>
-            <h2 className='text-center'>{randomString}</h2>
-            <button className='btn btn-primary ms-lg-5' onClick={() => {
-              const newString = arrayVerbs[randomNumber];
-              setRandomString(newString)
-              writeOneWord(newString)
-              setSingleWord(true)
-
-            }}>Next verb</button>
+    <div className="container">
+      {singleWord ? (
+        <div>
+          <div className="d-flex justify-content-center mt-5">
+            <h2>{randomWord}</h2>
+            <button
+              className="btn btn-primary ms-5"
+              onClick={() => {
+                setSingleWord(false);
+                const random = Math.floor(Math.random() * arrayVerbs.length);
+                setRandomNumber(random);
+                setAttempt('');
+              }}
+            >
+              View translate
+            </button>
           </div>
-          <input className='form-control my-4' type="text" placeholder='Write a sentence using this verb!' />
+          <input
+            className="form-control my-4 inputField"
+            type="text"
+            placeholder="Write the verb correctly!"
+            onKeyDown={(event) => handlerPressKey(event, false)}
+            ref={inputRef}
+            onChange={e => setAttempt( e.target.value)}
+          />
+          <p className="my-4 fs-1 text-center">{attempt}</p>
+        </div>
+      ) : (
+        <>
+          <div className="d-lg-flex justify-content-center mt-5 text-center">
+            <h2 className="text-center">{randomString}</h2>
+            <button
+              className="btn btn-primary ms-lg-5"
+              onClick={() => {
+                const newString = arrayVerbs[randomNumber];
+                setRandomString(newString);
+                writeOneWord(newString);
+                setSingleWord(true);
+              }}
+            >
+              Next verb
+            </button>
+          </div>
+          <input
+            className="form-control my-4 inputField"
+            type="text"
+            placeholder="Write a sentence using this verb!"
+            onKeyDown={(event) => handlerPressKey(event, true)}
+            ref={inputRef}
+          />
         </>
-      }
-
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default LearningVerbsComponent
+export default LearningVerbsComponent;
