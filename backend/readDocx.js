559d250b07890b4f7470b63ec3a6b76cd70bd6d7
Milos1988 <vojvoda1988@gmail.com>
2023-06-17 23:32:27 +0200

    finished learning component

--- backend/readDocx.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const mammoth = require('mammoth');
-
-async function readDocx(files) {
-  
-}
-
-module.exports = { readDocx }
--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,13 @@ app.use(cors())
 app.get('/verbs', async (req, res) => {
     const readTxt = await fs.promises.readFile('./initialFile/verbs.txt', "utf-8");
     const arrayText = readTxt.split('\n').map(line => line.trim());
-    const uniqueArray = Array.from(new Set(arrayText));
+    // const uniqueArray = Array.from(new Set(arrayText));
+    const uniqueArray = arrayText.filter((item, index) => {
+        const baseWord = item.split(' - ')[0].trim();
+        return arrayText.findIndex((elem) => elem.split(' - ')[0].trim() === baseWord) === index;
+      });
     res.json(uniqueArray)
-
 })
-
-
-
-
-
 
 app.listen(PORT, (error) => {
     error ? console.error("Error on server start") :
