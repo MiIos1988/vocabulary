d6dfaa13778eb22dcffc23edf82707c77552139c
Milos1988 <vojvoda1988@gmail.com>
2023-11-27 00:44:23 +0100

    Compression fail

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const PORT = 5500;
 const fs = require('fs');
 const cors = require('cors')
+const compression = require('compression');
 
 const app = express();
 
 app.use(cors())
+app.use(compression());
 
 app.get('/verbs', async (req, res) => {
     const readTxt = await fs.promises.readFile('./initialFile/verbs.txt', "utf-8");
     const arrayText = readTxt.split('\n').map(line => line.trim());
-    // const uniqueArray = Array.from(new Set(arrayText));
     const uniqueArray = arrayText.filter((item, index) => {
         const baseWord = item.split(' - ')[0].trim();
         return arrayText.findIndex((elem) => elem.split(' - ')[0].trim() === baseWord) === index;
